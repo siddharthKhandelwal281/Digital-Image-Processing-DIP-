d72b6b7f0b885c434ae581c19558a66a1c7acbb2
Siddharth Khandelwal <siddharthkhandelwal2683@gmail.com>
2026-01-26 15:05:05 +0530

    Create wavelet_transform.m

---
+++ Wavelet Transform/wavelet_transform.m
@@ -0,0 +1,45 @@
+%date - 26/01/2026 
+%created by - Siddharth Khandelwal
+clc ;
+clear all;
+close all;
+
+I =imread('fort_image.jpeg');
+imshow(I);
+title('Original Image');
+figure;
+Ig=rgb2gray(I);
+
+%subplot(2,3,2);
+imshow(Ig);
+title('Grayscale Image');
+figure;
+%doing the discrete wavelet transform 
+[Ia,Iv,Ih,Id]=dwt2(Ig,'haar');
+Ia=uint8(Ia);
+Iv=uint8(Iv);
+Ih=uint8(Ih);
+Id=uint8(Id);
+
+%subplot(2,3,3);
+imshow(Ia);
+title('Approximation (LL) Image - Ia');
+figure;
+%subplot(2,3,4);
+imshow(Iv);
+title('Vertical Detail (LH) Image - Iv');
+figure;
+%subplot(2,3,5);
+imshow(Ih);
+title('Horizontal Detail (HL) Image - Ih');
+figure;
+%subplot(2,3,6);
+imshow(Id);
+title('Diagonal Detail (HH) Image - Id');
+figure;
+
+%recovering the original image
+I_recover=idwt2(Ia,Iv,Ih,Id,'haar');
+I_recover = uint8(I_recover);
+imshow(I_recover);
+title('Recovered Image');
